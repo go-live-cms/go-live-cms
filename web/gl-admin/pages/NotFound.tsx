f7a1a77f4676f52da0e4fbcd59f869af62ebf418
Mateus Felipe <felipe.colla.m@gmail.com>
2025-08-18 18:23:50 +0200

    Added a not found page for the admin

---
+++ web/gl-admin/pages/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import "@gl-admin/assets/styles/pages/not-found.scss";
+
+const NotFound: React.FC = () => (
+    <div className="not-found">
+        <h1 className="not-found__title">404</h1>
+        <p className="not-found__message">Sorry, the page you are looking for does not exist.</p>
+        <Link to="/" className="not-found__link">
+            Go back to Dashboard
+        </Link>
+    </div>
+);
+
+export default NotFound;
