03ec197fdad8a56faebbafb421356eeb2b942309
Mateus Felipe <felipe.colla.m@gmail.com>
2025-08-27 03:31:19 +0200

    Added filterSelect component to content

--- web/gl-admin/pages/Content.tsx
+++ web/gl-admin/pages/Content.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Listing from "@gl-admin/layouts/Listing"
 import { getPosts } from "@gl-admin/lib/api/posts"
 import Table, { type TableColumnWithRender } from "@gl-admin/components/ui/Table"
@@ -7,6 +7,7 @@ import PostTitle from "@gl-admin/components/ui/PostTitle"
 import PostDateTime from "@gl-admin/components/ui/PostDateTime"
 import PostStatus from "@gl-admin/components/ui/PostStatus"
 import PostType from "@gl-admin/components/ui/PostType"
+import FilterSelect from "@gl-admin/components/ui/FilterSelect"
 import type { Post, ApiMeta } from "@gl-admin/lib/api/types"
 import type { PostQueryParams } from "@gl-admin/lib/api/posts"
 
@@ -16,6 +17,12 @@ type ContentProps = {
 }
 
 const Content: React.FC<ContentProps> = ({ query: queryProp, title }) => {
+
+    const [selectedFilters, setSelectedFilters] = useState<Record<string, string>>({
+        status: "",
+        type: "",
+        sort: "date_asc"
+    })
 
     const columns: TableColumnWithRender<Post>[] = [
         { key: "title", name: "Post", width: "34.8125rem", render: (_, row) => <PostTitle value={row} /> },
@@ -37,16 +44,55 @@ const Content: React.FC<ContentProps> = ({ query: queryProp, title }) => {
         })
     }
 
+    const filters = (
+        <>
+            <FilterSelect
+                options={[
+                    { label: "All status", value: "" },
+                    { label: "Published", value: "published" },
+                    { label: "Draft", value: "draft" },
+                ]}
+                value={selectedFilters.status}
+                onChange={value => setSelectedFilters({ ...selectedFilters, status: value })}
+            />
+            <FilterSelect
+                options={[
+                    { label: "All types", value: "" },
+                    { label: "Only posts", value: "post" },
+                    { label: "Only pages", value: "page" },
+                ]}
+                value={selectedFilters.type}
+                onChange={value => setSelectedFilters({ ...selectedFilters, type: value })}
+            />
+            <FilterSelect
+                options={[
+                    { label: "Created at", value: "date_asc" },
+                    { label: "Title", value: "title_asc" },
+                ]}
+                value={selectedFilters.sort}
+                onChange={value => setSelectedFilters({ ...selectedFilters, sort: value })}
+                prefix="Sort by:"
+            />
+        </>
+    )
+
+    const getQuery = () => {
+        return {
+            ...queryProp,
+            ...selectedFilters,
+        }
+    }
+
     const fetchData = async ({ limit, offset, ...query }: ApiMeta & PostQueryParams) => {
         const response = await getPosts({ limit, offset, with_meta: true, ...query })
         return { data: response.data, total: response.meta.total || 0 }
     }
 
     return (
-        <Listing title={title || "Content"}>
+        <Listing title={title || "Content"} actions={filters}>
             <Pagination
                 fetchData={fetchData}
-                query={queryProp || {}}
+                query={getQuery() || {}}
             >
                 {({ data, loading }) => <Table columns={columns} data={data} loading={loading} />}
             </Pagination>
