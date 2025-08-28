e0aaf298d961d1cb9f1cce9607bbe8f60312e17d
Mateus Felipe <felipe.colla.m@gmail.com>
2025-08-28 21:06:17 +0200

    Added different postStatus colors for dark mode

--- web/gl-admin/components/ui/PostStatus.tsx
+++ web/gl-admin/components/ui/PostStatus.tsx
@@ -5,15 +5,29 @@ import "@gl-admin/assets/styles/components/ui/post-status.scss";
 export default function PostStatus({ status, iconPath }: { status: string | null, iconPath: string | null }) {
 
     const getStatusColor = () => {
-        switch (status) {
-            case "published":
-                return "#0F5E0F";
-            case "draft":
-                return "#333536";
-            case "private":
-                return "#434343";
-            default:
-                return "black";
+        const isDark = document.documentElement.getAttribute("data-theme") === "dark";
+        if (isDark) {
+            switch (status) {
+                case "published":
+                    return "#71FF71";
+                case "draft":
+                    return "#FFDF60";
+                case "private":
+                    return "#888888";
+                default:
+                    return "#FFFFFF";
+            }
+        } else {
+            switch (status) {
+                case "published":
+                    return "#0F5E0F";
+                case "draft":
+                    return "#333536";
+                case "private":
+                    return "#434343";
+                default:
+                    return "black";
+            }
         }
     };
 
