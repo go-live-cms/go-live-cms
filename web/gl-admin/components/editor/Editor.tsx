51d5f7903cae13ac46913d3efab12757e367a943
Mateus Felipe <felipe.colla.m@gmail.com>
2025-09-10 14:43:13 +0200

    Updated dragHandle logic and styles

--- web/gl-admin/components/editor/Editor.tsx
+++ web/gl-admin/components/editor/Editor.tsx
@@ -309,10 +309,13 @@ export default function Editor({
       {/* Drag Handle */}
       <DragHandle editor={editor} onNodeChange={onDragHandleNodeChange}>
         <div
-          className="
-            flex items-center justify-center text-md text-white
-            mr-1.5 h-6 w-5 rounded-sm drag-handle cursor-grab
-            bg-gray-800 hover:bg-gray-700"
+          className={[
+            'flex items-center justify-center text-md text-white leading-none',
+            'h-6 w-5 mr-1.5 rounded border border-gray-600 cursor-grab select-none',
+            'bg-gray-800 hover:bg-gray-700 transition-delay-500',
+            'duration-200',
+            showDrag ? 'opacity-80 pointer-events-auto' : 'opacity-0 pointer-events-non',
+          ].join(' ')}
           title="Drag to move block"
           onMouseDown={(e) => (e.currentTarget.style.cursor = 'grabbing')}
           onMouseUp={(e) => (e.currentTarget.style.cursor = 'grab')}
