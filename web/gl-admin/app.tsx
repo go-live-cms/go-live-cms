18b5f28f711cf522d0fe11980e79490d5e46202e
Mateus Felipe <felipe.colla.m@gmail.com>
2025-09-15 17:11:24 +0200

    Added new users page

--- web/gl-admin/app.tsx
+++ web/gl-admin/app.tsx
@@ -13,6 +13,7 @@ const NotFound = lazy(() => import("@gl-admin/pages/NotFound"))
 const Dashboard = lazy(() => import("@gl-admin/pages/Dashboard"))
 const Content = lazy(() => import("@gl-admin/pages/Content"))
 const Media = lazy(() => import("@gl-admin/pages/Media"))
+const Users = lazy(() => import("@gl-admin/pages/Users"))
 
 function AppLayout() {
   const routeClasses = useRouteClasses()
@@ -39,6 +40,7 @@ function AppLayout() {
             <Route path="/content/pages/new" element={<NewPage />} />
             <Route path="/content/edit/:id" element={<EditContent />} />
             <Route path="/media" element={<Media />} />
+            <Route path="/users" element={<Users />} />
           </Routes>
         </div>
       </section>
