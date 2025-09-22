e6ceb9ec8cd7b35179a35e9dc391cc304e564ed3
Mateus Felipe <felipe.colla.m@gmail.com>
2025-09-22 23:22:29 +0200

    Removed unused css

--- web/gl-admin/pages/NewUser.tsx
+++ web/gl-admin/pages/NewUser.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import UserForm from "@gl-admin/components/forms/UserForm"
-import "@gl-admin/assets/styles/components/editor/post-editor.scss"
 
 export default function NewPost() {
     return <UserForm mode="create" />
