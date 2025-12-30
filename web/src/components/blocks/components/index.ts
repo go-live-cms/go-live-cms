533c7bf4562899c1f830912d4319bf7faceb8489
Luan Kraisch <52788268+luan-k@users.noreply.github.com>
2025-12-30 20:43:22 +0100

    feat: remove unused block components and their configurations

--- web/src/components/blocks/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Re-export all block components
-export { default as ParagraphBlock, paragraphConfig } from "./ParagraphBlock"
-export { default as HeadingBlock, headingConfig } from "./HeadingBlock"
-export { default as BlockquoteBlock, blockquoteConfig } from "./BlockquoteBlock"
-export { default as CodeBlock, codeBlockConfig } from "./CodeBlock"
-export { default as DividerBlock, dividerConfig } from "./DividerBlock"
-export { default as ImageBlock, imageConfig } from "./ImageBlock"
-export { default as BulletListBlock, bulletListConfig } from "./BulletListBlock"
-export { default as OrderedListBlock, orderedListConfig } from "./OrderedListBlock"
